e0a2070d9400401baaf4bfe3d7b2a8bd4cda8ebd
Laxmidevi12 <151540185+Laxmidevi12@users.noreply.github.com>
2026-05-14 20:26:24 +0530

    Update Project.js

--- Backend/models/Project.js
+++ Backend/models/Project.js
@@ -1,29 +1,50 @@
-const mongoose = require("mongoose");
+const mongoose =
+  require("mongoose");
 
-const projectSchema = new mongoose.Schema(
-{
-  title: {
-    type: String,
-    required: true
-  },
+const projectSchema =
+  new mongoose.Schema(
 
-  description: {
-    type: String
-  },
-
-  admin: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User"
-  },
-
-  members: [
     {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "User"
-    }
-  ]
-},
-{ timestamps: true }
-);
 
-module.exports = mongoose.model("Project", projectSchema);
+      title: {
+        type: String,
+        required: true
+      },
+
+      description: {
+        type: String
+      },
+
+      createdBy: {
+
+        type:
+          mongoose.Schema.Types.ObjectId,
+
+        ref: "User"
+
+      },
+
+      members: [
+
+        {
+          type:
+            mongoose.Schema.Types.ObjectId,
+
+          ref: "User"
+        }
+
+      ]
+
+    },
+
+    {
+      timestamps: true
+    }
+
+  );
+
+module.exports =
+  mongoose.model(
+    "Project",
+    projectSchema
+  );
